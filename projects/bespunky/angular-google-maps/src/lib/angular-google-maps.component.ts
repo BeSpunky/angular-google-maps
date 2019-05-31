328475cfb320be44834b246b6ce1063145cb180c
Shy Agam <us@bespunky.io>
2019-05-31 15:03:13 +0300

    :sparkles: Generated @bespunky/angular-google-maps library

---
+++ projects/bespunky/angular-google-maps/src/lib/angular-google-maps.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-angular-google-maps',
+  template: `
+    <p>
+      angular-google-maps works!
+    </p>
+  `,
+  styles: []
+})
+export class AngularGoogleMapsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/bespunky/angular-google-maps/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of angular-google-maps
+ */
+
+export * from './lib/angular-google-maps.service';
+export * from './lib/angular-google-maps.component';
+export * from './lib/angular-google-maps.module';
