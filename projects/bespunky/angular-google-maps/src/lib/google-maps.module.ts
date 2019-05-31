f4abdda69f581ca706463e3cd1afcae4eddafad0
Shy Agam <us@bespunky.io>
2019-06-01 02:44:36 +0300

    :heavy_plus_sign: Implemented GoogleMapsModule

--- projects/bespunky/angular-google-maps/src/lib/google-maps.module.ts
+++ projects/bespunky/angular-google-maps/src/lib/google-maps.module.ts
@@ -1,11 +1,60 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf, ModuleWithProviders } from '@angular/core';
 import { ZenModule } from '@bespunky/angular-zen';
+
+import { LazyGoogleMapsApiLoader } from './loaders/lazy-google-maps-api-loader';
+import { GoogleMapsApiLoader } from './loaders/google-maps-api-loader';
+import { GoogleMapsApiService } from './api/google-maps-api.service';
+
+export const DefaultApiUrlFormat = 'https://maps.googleapis.com/maps/api/js?key={apiKey}';
+
+export class GoogleMapsConfig
+{
+    public apiUrlFormat?: string;
+
+    public apiKey: string;
+}
 
 @NgModule({
     declarations: [],
     imports: [
         ZenModule
     ],
+    providers: [
+        GoogleMapsApiService,
+        { provide: GoogleMapsApiLoader, useClass: LazyGoogleMapsApiLoader }
+    ],
     exports: []
 })
-export class GoogleMapsModule { }
+export class GoogleMapsModule
+{
+    constructor(@Optional() @SkipSelf() googleMapsModule: GoogleMapsModule, private api: GoogleMapsApiService)
+    {
+        if (googleMapsModule)
+            throw new Error('GoogleMapsModule was previously loaded somewhere. Make sure there is only place where you import it.');
+
+        api.load().then(this.onApiLoaded)
+                  .catch(this.onApiLoadError);
+    }
+
+    static forRoot(config: GoogleMapsConfig): ModuleWithProviders
+    {
+        return {
+            ngModule: GoogleMapsModule,
+            providers: [
+                { provide: GoogleMapsConfig, useValue: config }
+            ]
+        };
+    }
+
+    private onApiLoaded()
+    {
+        // TODO: Notify anyone??
+    }
+
+    private onApiLoadError(error: any)
+    {
+        // TODO: Notify anyone??
+
+        console.log('[GoogleMapsModule] Google Maps API failed to load:', error);
+    }
+}
