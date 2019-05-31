7578df76cdf404fa1445f31d2522fcced41c8ab7
Shy Agam <us@bespunky.io>
2019-05-31 19:13:14 +0300

    :recycle: Renamed AngularGoogleMapsModule to GoogleMapsModule

--- projects/bespunky/angular-google-maps/src/lib/google-maps.module.ts
+++ projects/bespunky/angular-google-maps/src/lib/google-maps.module.ts
@@ -8,4 +8,4 @@ import { ZenModule } from '@bespunky/angular-zen';
     ],
     exports: []
 })
-export class AngularGoogleMapsModule { }
+export class GoogleMapsModule { }
--- projects/bespunky/angular-google-maps/src/public-api.ts
+++ projects/bespunky/angular-google-maps/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of angular-google-maps
  */
 
-export * from './lib/angular-google-maps.service';
-export * from './lib/angular-google-maps.component';
-export * from './lib/angular-google-maps.module';
+export * from './lib/google-maps.module';
