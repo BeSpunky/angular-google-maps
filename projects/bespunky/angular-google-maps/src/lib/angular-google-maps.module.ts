7902ef9288bdd12f87e9a35261465a50f359ff06
Shy Agam <us@bespunky.io>
2019-05-31 19:07:22 +0300

    :heavy_plus_sign: Added ZenModule import

--- projects/bespunky/angular-google-maps/src/lib/angular-google-maps.module.ts
+++ projects/bespunky/angular-google-maps/src/lib/angular-google-maps.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { ZenModule } from '@bespunky/angular-zen';
+
 import { AngularGoogleMapsComponent } from './angular-google-maps.component';
 
 @NgModule({
     declarations: [AngularGoogleMapsComponent],
     imports: [
+        ZenModule
     ],
     exports: [AngularGoogleMapsComponent]
 })
