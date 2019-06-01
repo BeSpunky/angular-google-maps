09b6ce466a995bafdbf90a13f35c7ea2852dae82
Shy Agam <us@bespunky.io>
2019-06-01 14:38:41 +0300

    :heavy_plus_sign: Added missing exports to public api

--- projects/bespunky/angular-google-maps/src/public-api.ts
+++ projects/bespunky/angular-google-maps/src/public-api.ts
@@ -3,3 +3,8 @@
  */
 
 export * from './lib/google-maps.module';
+export * from './lib/google-maps-config';
+export * from './lib/api/google-maps-api.service';
+export * from './lib/loaders/google-maps-api-loader';
+export * from './lib/loaders/lazy-google-maps-api-loader';
+export * from './lib/loaders/no-op-google-maps-api-loader';
