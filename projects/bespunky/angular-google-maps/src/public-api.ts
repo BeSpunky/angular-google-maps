6e862cc4a788b93f5e3abe6e3fe3131c600446f7
Shy Agam <us@bespunky.io>
2019-06-01 17:49:19 +0300

    ➕ Added new public api and formatted code

--- projects/bespunky/angular-google-maps/src/public-api.ts
+++ projects/bespunky/angular-google-maps/src/public-api.ts
@@ -2,9 +2,16 @@
  * Public API Surface of angular-google-maps
  */
 
+// Modules
 export * from './lib/google-maps.module';
-export * from './lib/google-maps-config';
+
+// Providers
 export * from './lib/api/google-maps-api.service';
+export * from './lib/config/google-maps-config';
 export * from './lib/loaders/google-maps-api-loader';
 export * from './lib/loaders/lazy-google-maps-api-loader';
 export * from './lib/loaders/no-op-google-maps-api-loader';
+
+// Classes & Enums
+export * from './lib/config/defaults';
+export * from './lib/types/zoom-level.enum';
