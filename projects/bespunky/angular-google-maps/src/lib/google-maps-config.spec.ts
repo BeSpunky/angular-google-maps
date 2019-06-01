37ab67fe6427b4e93374754fb785c1816b8d1fa3
Shy Agam <us@bespunky.io>
2019-06-01 13:39:24 +0300

    :recycle: Refactored configuration and moved to a new file

---
+++ projects/bespunky/angular-google-maps/src/lib/google-maps-config.spec.ts
@@ -0,0 +1,9 @@
+import { GoogleMapsConfig } from './google-maps-config';
+
+describe('GoogleMapsConfig', () =>
+{
+    it('should create an instance', () =>
+    {
+        expect(new GoogleMapsConfig()).toBeTruthy();
+    });
+});
--- projects/bespunky/angular-google-maps/src/lib/google-maps.module.ts
+++ projects/bespunky/angular-google-maps/src/lib/google-maps.module.ts
@@ -1,24 +1,14 @@
-import { NgModule, Optional, SkipSelf, ModuleWithProviders } from '@angular/core';
+import { NgModule, Optional, SkipSelf, ModuleWithProviders, NgZone } from '@angular/core';
 import { ZenModule } from '@bespunky/angular-zen';
 
 import { LazyGoogleMapsApiLoader } from './loaders/lazy-google-maps-api-loader';
 import { GoogleMapsApiLoader } from './loaders/google-maps-api-loader';
 import { GoogleMapsApiService } from './api/google-maps-api.service';
-
-export const DefaultApiUrlFormat = 'https://maps.googleapis.com/maps/api/js?key={apiKey}';
-
-export class GoogleMapsConfig
-{
-    public apiUrlFormat?: string;
-
-    public apiKey: string;
-}
+import { GoogleMapsConfig } from './google-maps-config';
 
 @NgModule({
     declarations: [],
-    imports: [
-        ZenModule
-    ],
+    imports: [ ZenModule ],
     providers: [
         GoogleMapsApiService,
         { provide: GoogleMapsApiLoader, useClass: LazyGoogleMapsApiLoader }
@@ -27,13 +17,16 @@ export class GoogleMapsConfig
 })
 export class GoogleMapsModule
 {
-    constructor(@Optional() @SkipSelf() googleMapsModule: GoogleMapsModule, private api: GoogleMapsApiService)
+    constructor(@Optional() @SkipSelf() googleMapsModule: GoogleMapsModule, private zone: NgZone, private api: GoogleMapsApiService)
     {
         if (googleMapsModule)
             throw new Error('GoogleMapsModule was previously loaded somewhere. Make sure there is only place where you import it.');
 
-        api.load().then(this.onApiLoaded)
-                  .catch(this.onApiLoadError);
+        zone.runOutsideAngular(() =>
+        {
+            api.load().then(this.onApiLoaded)
+                      .catch(this.onApiLoadError);
+        });
     }
 
     static forRoot(config: GoogleMapsConfig): ModuleWithProviders
