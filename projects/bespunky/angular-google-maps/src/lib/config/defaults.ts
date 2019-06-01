ebf4f6d0583f0524c448337d8474044162dce584
Shy Agam <us@bespunky.io>
2019-06-01 18:23:20 +0300

    🐛 Fixed default center object causing runtime error

--- projects/bespunky/angular-google-maps/src/lib/config/defaults.ts
+++ projects/bespunky/angular-google-maps/src/lib/config/defaults.ts
@@ -2,6 +2,6 @@ import { ZoomLevel } from '../types/zoom-level.enum';
 
 export class Defaults
 {
-    public static readonly Center = new google.maps.LatLng(31.852775, 34.818484);
+    public static readonly Center = { lat: 31.852775, lng: 34.818484 };
     public static readonly ZoomLevel = ZoomLevel.City;
 }
