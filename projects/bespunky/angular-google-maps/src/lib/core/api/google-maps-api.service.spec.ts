76b39cd3abcf9e6dd8a48143576c5fd60ecc5728
Shy Agam <us@bespunky.io>
2019-06-06 13:19:23 +0300

    ✅ Added testing for GoogleMapsApiService

--- projects/bespunky/angular-google-maps/src/lib/core/api/google-maps-api.service.spec.ts
+++ projects/bespunky/angular-google-maps/src/lib/core/api/google-maps-api.service.spec.ts
@@ -1,14 +1,86 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GoogleMapsApiService } from './google-maps-api.service';
+import { GoogleMapsModule } from '../../google-maps.module';
+import { GoogleMapsConfig } from '../config/google-maps-config';
+import { BehaviorSubject } from 'rxjs';
+import { GoogleMapsApiReadyPromise } from './google-maps-api-ready.token';
+import { NgZone } from '@angular/core';
 
 describe('GoogleMapsApiService', () =>
 {
-    beforeEach(() => TestBed.configureTestingModule({}));
+    let config: GoogleMapsConfig;
+    let service: GoogleMapsApiService;
+    let waitForApiPromiseCreation: BehaviorSubject<Promise<void>>;
+    let tokenSubscribeSpy: jasmine.Spy;
 
-    // it('should be created', () =>
-    // {
-    //     const service: GoogleMapsApiService = TestBed.get(GoogleMapsApiService);
-    //     expect(service).toBeTruthy();
-    // });
+    beforeEach(() =>
+    {
+        const waitToken = new BehaviorSubject<Promise<void>>(null);
+
+        tokenSubscribeSpy = spyOn(waitToken, 'subscribe').and.callThrough();
+
+        config = { apiUrl: { key: 'dummykey' } };
+
+        TestBed.configureTestingModule({
+            imports: [GoogleMapsModule.forRoot(config)],
+            providers: [
+                { provide: GoogleMapsApiReadyPromise, useValue: waitToken }
+            ]
+        });
+
+        waitForApiPromiseCreation = TestBed.get(GoogleMapsApiReadyPromise);
+
+        service = TestBed.get(GoogleMapsApiService);
+    });
+
+    it('should create an instance', () => expect(service).toBeTruthy());
+
+    it('should subscribe to the api promise creation observable and store the `mapsApiReady` promise', () =>
+    {
+        expect(tokenSubscribeSpy).toHaveBeenCalledTimes(1);
+
+        waitForApiPromiseCreation.next(new Promise(() => ({})));
+
+        expect((service as any).mapsApiReady instanceof Promise).toBeTruthy();
+    });
+
+    it('should allow retrieving the promise easily using the `whenReady` getter', () =>
+    {
+        const promise = new Promise<void>(() => ({}));
+
+        waitForApiPromiseCreation.next(promise);
+
+        expect(service.whenReady).toBe(promise);
+    });
+
+    it('should unsubscribe from the api promise creation observable on destroy', () =>
+    {
+        spyOn(waitForApiPromiseCreation, 'unsubscribe');
+
+        service.ngOnDestroy();
+
+        expect(waitForApiPromiseCreation.unsubscribe).toHaveBeenCalledTimes(1);
+    });
+
+    it('should allow running given function outside of angular when the maps api is ready', (done: DoneFn) =>
+    {
+        const promise = new Promise<void>((resolve) => resolve());
+
+        waitForApiPromiseCreation.next(promise);
+
+        const zone = TestBed.get(NgZone);
+
+        const whenReadySpy = spyOnProperty(service, 'whenReady', 'get').and.callThrough();
+
+        spyOn(zone, 'runOutsideAngular').and.callFake((fn: () => void) => fn());
+
+        // This will call the fake `runOutsideAngular()` function which will have the `done()` function execute when the api promise resolves.
+        // If the api promise doesn't resolve, `done()` will never be called and the test will fail.
+        service.runOutsideAngular(done);
+
+        // Expect the promise to be fetched and NgZone's function to be executed on the way to glory
+        expect(whenReadySpy).toHaveBeenCalledTimes(1);
+        expect(zone.runOutsideAngular).toHaveBeenCalledTimes(1);
+    });
 });
