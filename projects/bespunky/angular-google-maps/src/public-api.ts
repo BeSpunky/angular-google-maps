ec4e7270a7b9b54d684e956c651cdda3ba5bd7d2
Shy Agam <us@bespunky.io>
2020-03-19 17:16:41 +0200

    ➕ Added missing public exports

--- projects/bespunky/angular-google-maps/src/public-api.ts
+++ projects/bespunky/angular-google-maps/src/public-api.ts
@@ -12,17 +12,25 @@ export * from './lib/core/loaders/google-maps-api-loader';
 export * from './lib/core/loaders/lazy-google-maps-api-loader';
 export * from './lib/core/loaders/no-op-google-maps-api-loader';
 
+export * from './lib/utils/transform/event-data-transform.service';
+
 // Classes & Enums
 export * from './lib/core/config/defaults';
+export * from './lib/core/abstraction/events/google-maps-event-data';
 
 // Map
-export * from './lib/google-map/component/google-map.component';
 export * from './lib/google-map/google-map';
+export * from './lib/google-map/component/google-map.component';
 export * from './lib/google-map/types/zoom-level.enum';
 export * from './lib/google-map/types/map-event.enum';
 
 // Markers
-export * from './lib/overlays/marker/directive/google-maps-marker.directive';
 export * from './lib/overlays/marker/google-maps-marker';
+export * from './lib/overlays/marker/directive/google-maps-marker.directive';
 export * from './lib/overlays/marker/types/marker-event.enum';
+
+// Data Layer
+export * from './lib/overlays/data/google-maps-data';
+export * from './lib/overlays/data/directive/google-maps-data.directive';
+export * from './lib/overlays/data/types/data-event.enum';
 
