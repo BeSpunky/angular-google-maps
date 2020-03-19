a9e4f8a83e9eae219ff8fab25f3f49f1ba543a66
Shy Agam <us@bespunky.io>
2020-03-19 22:57:02 +0200

    ✅ Adapted GoogleMap class testing

--- projects/bespunky/angular-google-maps/src/lib/google-map/google-map.spec.ts
+++ projects/bespunky/angular-google-maps/src/lib/google-map/google-map.spec.ts
@@ -7,6 +7,7 @@ import { GoogleMap } from './google-map';
 import { Defaults } from '../core/config/defaults';
 import { ZoomLevel } from './types/zoom-level.enum';
 import { GoogleMapsMarker } from '../overlays/marker/google-maps-marker';
+import { IGoogleMapsDrawableOverlay } from '../core/abstraction/base/i-google-maps-drawable-overlay';
 
 const elementStub: any = document.createElement('div');
 
@@ -35,33 +36,26 @@ describe('GoogleMap', () =>
 
         it('should wait for api and create a native map outside of angular with default center and zoom', async () =>
         {
-            await api.whenReady;
-
             const nativeMap = await map.native;
 
             expect(runOutsideAngularSpy).toHaveBeenCalledTimes(1);
             expect(nativeMap instanceof google.maps.Map).toBeTruthy();
 
-            expect(nativeMap.getCenter().lat()).toBeCloseTo(Defaults.Center.lat, 6);
-            expect(nativeMap.getCenter().lng()).toBeCloseTo(Defaults.Center.lng, 6);
+            expectPositionEquals(nativeMap.getCenter(), Defaults.Center);
             expect(nativeMap.getZoom()).toBe(Defaults.ZoomLevel);
         });
 
         it('should allow creation of the native map with specific center and zoom', async () =>
         {
-            await api.whenReady;
-
             const customCenter = { lat: 10, lng: 10 };
             const customZoom = ZoomLevel.World;
 
             const customMap = new GoogleMap(mapElement, api, customCenter, customZoom);
             const nativeMap = await customMap.native;
 
-            expectPositionEquals(await nativeMap.getCenter(), customCenter);
+            expectPositionEquals(nativeMap.getCenter(), customCenter);
             expect(nativeMap.getZoom()).toBe(customZoom);
         });
-
-        it('should wait for the api and return the native map when calling the `native` getter', async () => expect(await map.native).toBeDefined());
     });
 
     describe('upon calling get functions', () =>
@@ -98,9 +92,35 @@ describe('GoogleMap', () =>
 
     describe('overlay management', () =>
     {
+        const dummyOverlay = {};
+
         beforeEach(() => runOutsideAngularSpy.calls.reset());
 
-        it('should create a marker associated with the map outside angular when calling `createMarker()`', async () =>
+        it('should create an overlay outside of angular and add it to the overlay tracker when calling `createOverlay()`', async () =>
+        {
+            const overlayTrackerSpy = spyOn(map.overlays, 'add');
+            
+            const createOverlay: (createObject: () => any) => Promise<any> = (map as any).createOverlay;
+
+            const overlay = await createOverlay(() => dummyOverlay);
+
+            expect(runOutsideAngularSpy).toHaveBeenCalledTimes(1);
+            expect(overlayTrackerSpy).toHaveBeenCalledTimes(1);
+            expect(overlay).toBe(dummyOverlay);
+        });
+
+        it('should remove an overlay outside of angular and remove it to the overlay tracker when calling `removeOverlay()`', async () =>
+        {
+            // There should be an overlay stored in the tracker from the previous test
+            const overlayTrackerSpy = spyOn(map.overlays, 'remove');
+            
+            await map.removeOverlay(dummyOverlay as IGoogleMapsDrawableOverlay);
+
+            expect(runOutsideAngularSpy).toHaveBeenCalledTimes(1);
+            expect(overlayTrackerSpy).toHaveBeenCalledTimes(1);
+        });
+
+        it('should create a marker associated with the map when calling `createMarker()`', async () =>
         {
             const options: google.maps.ReadonlyMarkerOptions = {
                 position: Defaults.Center
@@ -108,8 +128,8 @@ describe('GoogleMap', () =>
 
             const marker = await map.createMarker(options);
 
-            expect(runOutsideAngularSpy).toHaveBeenCalledTimes(2); // Once by createMarker(), once by the inherited GoogleMapsDrawableOverlay calling setContainingMap()
             expect(marker instanceof GoogleMapsMarker).toBeTruthy();
+            expect(marker.map).toBe(map);
         });
     });
 });
