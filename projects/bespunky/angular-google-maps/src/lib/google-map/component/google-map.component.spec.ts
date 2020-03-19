15f3649f9b0ba29fa242fd3e658c1afbde5d7b9f
Shy Agam <us@bespunky.io>
2020-03-19 23:40:02 +0200

    ✅ Removed redundant tests

--- projects/bespunky/angular-google-maps/src/lib/google-map/component/google-map.component.spec.ts
+++ projects/bespunky/angular-google-maps/src/lib/google-map/component/google-map.component.spec.ts
@@ -6,27 +6,16 @@ import { GoogleMapComponent } from './google-map.component';
 import { createDefaultTestModuleConfig } from '../../testing/utils';
 import { GoogleMapsApiService } from '../../core/api/google-maps-api.service';
 import { GoogleMapsInternalApiService } from '../../core/api/google-maps-internal-api.service';
-import { GoogleMapsEventData } from '../../core/abstraction/events/google-maps-event-data';
 import { GoogleMap } from '../google-map';
-import { MapEvent } from '../types/map-event.enum';
 
-@Component({
-    template: '<bs-google-map [map]="map" [center]="center" (click)="onClick($event)"></bs-google-map>'
-})
-class TestHostComponent
-{
-    @ViewChild(GoogleMapComponent)
-    public mapComponent: GoogleMapComponent;
-
-    public map: GoogleMap;
-    public center: google.maps.LatLng;
-
-    public onClick(eventData: GoogleMapsEventData)
-    {
-        return true;
-    }
-}
-
+/**
+ * -- NOTE --
+ * Events hooking and property delegation are not tested in components deriving from `GoogleMapsLifecycleBase`.
+ * The appropriate tests are already done by `GoogleMapsLifecycleBase` and `GoogleMapsInternalApiService`/
+ * 
+ * @see `google-maps-internal-api.service.spec.ts` For testing of the hooking and delegation mechanisms.
+ * @see `google-maps-lifecycle-base.spec.ts` For testing of the integration between the component and the internal API service.
+ */
 describe('GoogleMapComponent', () =>
 {
     let hostFixture: ComponentFixture<TestHostComponent>;
@@ -79,36 +68,16 @@ describe('GoogleMapComponent', () =>
         expect(component.map).toBeUndefined();
         expect((component as any).initNativeWrapper() instanceof GoogleMap).toBeTruthy();
     });
-
-    it('should delegate changes to the native map', async () =>
-    {
-        // Get the map object and spy on its setter method
-        const map = component.nativeWrapper as GoogleMap;
-        spyOn(map, 'setCenter').and.callThrough();
-
-        // Set a new setter to the host
-        hostComponent.center = new google.maps.LatLng(10, 10);
-
-        hostFixture.detectChanges();
-
-        await hostFixture.whenStable();
-
-        expect(component.center).toBe(hostComponent.center);
-        expect(map.setCenter).toHaveBeenCalledTimes(2); // Once for first value (undefined), once for new value
-        expect(map.setCenter).toHaveBeenCalledWith(hostComponent.center);
-    });
-
-    it('should emit events when native objects raise events', async () =>
-    {
-        // The click event is the one being tested as it is the only one bound in the TestHostComponent's template.
-        // To test more events, add handlers to the tempalte.
-        
-        const map = await component.nativeWrapper.native;
-
-        spyOn(component.click, 'emit');
-
-        new google.maps.event.trigger(map, MapEvent.Click);
-
-        expect(component.click.emit).toHaveBeenCalled();
-    });
 });
+
+@Component({
+    template: '<bs-google-map [map]="map" [center]="center"></bs-google-map>'
+})
+class TestHostComponent
+{
+    @ViewChild(GoogleMapComponent)
+    public mapComponent: GoogleMapComponent;
+
+    public map: GoogleMap;
+    public center: google.maps.LatLng;
+}
