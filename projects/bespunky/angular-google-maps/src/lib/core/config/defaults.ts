755662b8014763d1d587e088980f2157794812ca
Shy Agam <us@bespunky.io>
2020-04-03 17:58:13 +0300

    🔧 Changed default map center

--- projects/bespunky/angular-google-maps/src/lib/core/config/defaults.ts
+++ projects/bespunky/angular-google-maps/src/lib/core/config/defaults.ts
@@ -2,6 +2,6 @@ import { ZoomLevel } from '../../google-map/types/zoom-level.enum';
 
 export class Defaults
 {
-    public static readonly Center = { lat: 31.852775, lng: 34.818484 };
+    public static readonly Center = { lat: 0, lng: 0 };
     public static readonly ZoomLevel = ZoomLevel.City;
 }
