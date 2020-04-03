7b78890383026526445798ff7bc36dde63b58d4e
Shy Agam <us@bespunky.io>
2020-04-03 11:35:24 +0300

    ✅ Adapted tests for GoogleMapsDataDirective

--- projects/bespunky/angular-google-maps/src/lib/overlays/data/directive/google-maps-data.directive.spec.ts
+++ projects/bespunky/angular-google-maps/src/lib/overlays/data/directive/google-maps-data.directive.spec.ts
@@ -1,37 +1,37 @@
-// import { Component } from '@angular/core';
-// import { ComponentFixture } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ComponentFixture } from '@angular/core/testing';
 
-// import { GoogleMapsDataDirective } from './google-maps-data.directive';
-// import { configureGoogleMapsTestingModule } from '../../../testing/setup.spec';
-// import { LifecycleTestsHostComponentBase, createLifecycleTestingHostComponentTemplate } from '../../../testing/lifecycle-components.spec';
+import { GoogleMapsDataDirective } from './google-maps-data.directive';
+import { configureGoogleMapsTestingModule } from '../../../testing/setup.spec';
+import { createLifecycleTestingHostComponentTemplate, LifecycleComponentTestHost } from '../../../testing/lifecycle-components.spec';
 
-// /**
-//  * -- NOTE --
-//  * Events hooking and property delegation are not tested in components deriving from `GoogleMapsLifecycleBase`.
-//  * The appropriate tests are already done by `GoogleMapsLifecycleBase` and `GoogleMapsInternalApiService`/
-//  * 
-//  * @see `google-maps-internal-api.service.spec.ts` For testing of the hooking and delegation mechanisms.
-//  * @see `google-maps-lifecycle-base.spec.ts` For testing of the integration between the component and the internal API service.
-//  */
-// describe('GoogleMapsDataDirective', () =>
-// {
-//     let hostFixture: ComponentFixture<TestHostComponent>;
-//     let hostComponent: TestHostComponent;
-//     let directive: GoogleMapsDataDirective;
+/**
+ * -- NOTE --
+ * Events hooking and property delegation are not tested in components deriving from `GoogleMapsLifecycleBase`.
+ * The appropriate tests are already done by `GoogleMapsLifecycleBase` and `GoogleMapsInternalApiService`/
+ * 
+ * @see `google-maps-internal-api.service.spec.ts` For testing of the hooking and delegation mechanisms.
+ * @see `google-maps-lifecycle-base.spec.ts` For testing of the integration between the component and the internal API service.
+ */
+describe('GoogleMapsDataDirective', () =>
+{
+    let hostFixture: ComponentFixture<TestHostComponent>;
+    let hostComponent: TestHostComponent;
+    let directive: GoogleMapsDataDirective;
 
-//     beforeEach(async () =>
-//     {
-//         ({ fixture: hostFixture, component: hostComponent } = await configureGoogleMapsTestingModule({ componentType: TestHostComponent }));
+    beforeEach(async () =>
+    {
+        ({ fixture: hostFixture, component: hostComponent } = await configureGoogleMapsTestingModule({ componentType: TestHostComponent }));
         
-//         hostFixture.detectChanges();
+        hostFixture.detectChanges();
 
-//         directive = hostComponent.testedComponent as GoogleMapsDataDirective;
-//     });
+        directive = hostComponent.testedComponent as GoogleMapsDataDirective;
+    });
 
-//     it('should create an instance', () => expect(directive).toBeTruthy());
-// });
+    it('should create an instance', () => expect(directive).toBeTruthy());
+});
 
-// @Component({
-//     template: createLifecycleTestingHostComponentTemplate('<bs-google-maps-data #testedComponent></bs-google-maps-data>'),
-// })
-// class TestHostComponent extends LifecycleTestsHostComponentBase { }
+@Component({
+    template: createLifecycleTestingHostComponentTemplate('<bs-google-maps-data #testedComponent></bs-google-maps-data>'),
+})
+class TestHostComponent extends LifecycleComponentTestHost { }
