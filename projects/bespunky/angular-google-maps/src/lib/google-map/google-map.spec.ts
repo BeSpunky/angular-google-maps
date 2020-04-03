1b2462b255a4ec7ac27e16ff7ea5be0b739a76b4
Shy Agam <us@bespunky.io>
2020-04-03 18:00:55 +0300

    ✅ Adapted tests for GoogleMap

--- projects/bespunky/angular-google-maps/src/lib/google-map/google-map.spec.ts
+++ projects/bespunky/angular-google-maps/src/lib/google-map/google-map.spec.ts
@@ -1,96 +1,91 @@
-// import { ElementRef } from '@angular/core';
+import { ElementRef } from '@angular/core';
 
-// import { configureGoogleMapsTestingModule } from '../testing/setup.spec';
-// import { expectPositionEquals } from '../testing/expectations.spec';
-// import { GoogleMapsApiService } from '../core/api/google-maps-api.service';
-// import { GoogleMap } from './google-map';
-// import { Defaults } from '../core/config/defaults';
-// import { ZoomLevel } from './types/zoom-level.enum';
-// import { GoogleMapsMarker } from '../overlays/marker/google-maps-marker';
-// import { DrawableOverlay } from '../core/abstraction/types/drawable-overlay.type';
+import { configureGoogleMapsTestingModule } from '../testing/setup.spec';
+import { expectPositionEquals } from '../testing/expectations.spec';
+import { GoogleMapsApiService } from '../core/api/google-maps-api.service';
+import { GoogleMap } from './google-map';
+import { Defaults } from '../core/config/defaults';
+import { GoogleMapsMarker } from '../overlays/marker/google-maps-marker';
+import { GoogleMapsData } from '../overlays/data/google-maps-data';
 
-// const elementStub: any = document.createElement('div');
+const elementStub: any = document.createElement('div');
 
-// describe('GoogleMap', () =>
-// {
-//     let map: GoogleMap;
-//     let api: GoogleMapsApiService;
-//     let mapElement: ElementRef;
-//     let runOutsideAngular: jasmine.Spy;
+describe('GoogleMap', () =>
+{
+    let map              : GoogleMap;
+    let api              : GoogleMapsApiService;
+    let mapElement       : ElementRef;
+    let runOutsideAngular: jasmine.Spy;
 
-//     beforeEach(async () =>
-//     {
-//         ({ api, spies: { runOutsideAngular } } = await configureGoogleMapsTestingModule());
+    beforeEach(async () =>
+    {
+        ({ api, spies: { runOutsideAngular } } = await configureGoogleMapsTestingModule());
 
-//         mapElement = new ElementRef(elementStub);
-//         map = new GoogleMap(api, mapElement);
-//     });
+        mapElement = new ElementRef(elementStub);
+        map        = new GoogleMap(api, mapElement);
+    });
 
-//     describe('basically', () =>
-//     {
-//         it('should create an instance', () => expect(map).toBeTruthy());
+    describe('basically', () =>
+    {
+        it('should create an instance', () => expect(map).toBeTruthy());
 
-//         it('should wait for api and create a native map outside of angular with default center and zoom', async () =>
-//         {
-//             const nativeMap = await map.native;
+        it('should instantiate a new native map with the default zoom and center', () =>
+        {
+            expectPositionEquals(map.getCenter(), Defaults.Center);
+            expect(map.getZoom()).toBe(Defaults.ZoomLevel);
+        });
 
-//             expect(runOutsideAngular).toHaveBeenCalledTimes(1);
-//             expect(nativeMap instanceof google.maps.Map).toBeTruthy();
+        it('should allow instantiating the native map with custom options', () =>
+        {
+            const customMap = new GoogleMap(api, mapElement, {
+                center: new google.maps.LatLng({ lat: 2, lng: 2 }),
+                zoom: 999
+            });
 
-//             expectPositionEquals(nativeMap.getCenter(), Defaults.Center);
-//             expect(nativeMap.getZoom()).toBe(Defaults.ZoomLevel);
-//         });
+            expect(customMap).toBeDefined();
+            expectPositionEquals(customMap.getCenter(), { lat: 2, lng: 2 });
+            expect(customMap.getZoom()).toBe(999);
+        });
+    });
 
-//         it('should allow creation of the native map with specific center and zoom', async () =>
-//         {
-//             const center = { lat: 10, lng: 10 };
-//             const zoom = ZoomLevel.World;
-
-//             const customMap = new GoogleMap(api, mapElement, { center, zoom });
-//             const nativeMap = await customMap.native;
-
-//             expectPositionEquals(nativeMap.getCenter(), center);
-//             expect(nativeMap.getZoom()).toBe(zoom);
-//         });
-//     });
-
-//     describe('overlay management', () =>
-//     {
-//         const dummyOverlay = jasmine.createSpyObj('overlay', ['setContainingMap', 'removeFromMap']);
-
-//         beforeEach(() => runOutsideAngular.calls.reset());
-
-//         it('should create an overlay outside of angular and add it to the overlay tracker when calling `createOverlay()`', async () =>
-//         {
-//             const overlayTrackerSpy = spyOn(map.overlays, 'add');
+    describe('overlay management', () =>
+    {
+        it('should create a marker outside angular when calling `createMarker()`', () => 
+        {            
+            const marker = map.createMarker(Defaults.Center);
             
-//             // Get a hold of the private `createOverlay()` method and strong type it
-//             const createOverlay = (map as any).createOverlay as (createObject: () => any) => Promise<any>;
+            // Overlay creation ends up in more than one calls to runOutsideAngular().
+            // In order to avoid changing the test when changing implementations, call count is avoided and this is used instead.
+            expect(runOutsideAngular.calls.all().some(call => call.returnValue instanceof GoogleMapsMarker)).toBeTruthy();
 
-//             const overlay = await createOverlay.call(map, () => dummyOverlay);
+            expect(marker instanceof GoogleMapsMarker).toBeTruthy();
+            expectPositionEquals(marker.getPosition(), Defaults.Center);
+        });
+        
+        it('should create a data layer outside angular when calling `createDataLayer()`', () => 
+        {
+            const data = map.createDataLayer({ style: { title: 'awesome' } });
 
-//             expect(runOutsideAngular).toHaveBeenCalledTimes(1);
-//             expect(overlayTrackerSpy).toHaveBeenCalledTimes(1);
-//             expect(overlay).toBe(dummyOverlay);
-//         });
+            // Overlay creation ends up in more than one calls to runOutsideAngular().
+            // In order to avoid changing the test when changing implementations, call count is avoided and this is used instead.
+            expect(runOutsideAngular.calls.all().some(call => call.returnValue instanceof GoogleMapsData)).toBeTruthy();
 
-//         it('should remove an overlay outside of angular and remove it to the overlay tracker when calling `removeOverlay()`', async () =>
-//         {
-//             // There should be an overlay stored in the tracker from the previous test
-//             const overlayTrackerSpy = spyOn(map.overlays, 'remove');
+            expect(data instanceof GoogleMapsData).toBeTruthy();
+            expect((data.getStyle() as any).title).toBe('awesome');
+        });
 
-//             await map.removeOverlay(dummyOverlay as DrawableOverlay);
+        it('should remove an overlay outside angular when calling `removeOverlay()`', () =>
+        {
+            const marker = map.createMarker({ lat: 20, lng: 20 });
 
-//             expect(runOutsideAngular).toHaveBeenCalledTimes(1);
-//             expect(overlayTrackerSpy).toHaveBeenCalledTimes(1);
-//         });
+            expect(map.overlays.markers.includes(marker)).toBeTruthy();
 
-//         it('should create a marker associated with the map when calling `createMarker()`', async () =>
-//         {
-//             const marker = await map.createMarker(Defaults.Center);
+            runOutsideAngular.calls.reset();
 
-//             expect(marker instanceof GoogleMapsMarker).toBeTruthy();
-//             expect(marker.map).toBe(map);
-//         });
-//     });
-// });
+            map.removeOverlay(marker);
+
+            expect(runOutsideAngular.calls.count()).toBeGreaterThan(0);
+            expect(map.overlays.markers.includes(marker)).toBeFalsy();
+        });
+    });
+});
