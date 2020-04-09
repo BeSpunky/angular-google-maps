513991382e69ea12e93700e014c568fb728adae8
Shy Agam <us@bespunky.io>
2020-04-09 18:01:57 +0300

    ✅ Adapted tests for GoogleMapsMarker

--- projects/bespunky/angular-google-maps/src/lib/overlays/marker/google-maps-marker.spec.ts
+++ projects/bespunky/angular-google-maps/src/lib/overlays/marker/google-maps-marker.spec.ts
@@ -1,32 +1,22 @@
-// import { ElementRef } from '@angular/core';
+import { configureGoogleMapsTestingModule } from '../../testing/setup.spec';
+import { GoogleMapsMarker } from './google-maps-marker';
+import { GoogleMapsApiService } from '../../core/api/google-maps-api.service';
+import { MockGoogleMap } from '../../google-map/testing/mock-google-map.spec';
 
-// import { configureGoogleMapsTestingModule } from '../../testing/setup.spec';
-// import { GoogleMapsMarker } from './google-maps-marker';
-// import { GoogleMapsApiService } from '../../core/api/google-maps-api.service';
-// import { GoogleMap } from '../../google-map/google-map';
+describe('GoogleMapsMarker', () =>
+{
+    let api: GoogleMapsApiService;
+    let marker: GoogleMapsMarker;
 
-// const elementStub = document.createElement('div');
+    beforeEach(async () =>
+    {
+        ({ api } = await configureGoogleMapsTestingModule());
 
-// describe('GoogleMapsMarker', () =>
-// {
-//     let api: GoogleMapsApiService;
-//     let runOutsideAngular: jasmine.Spy;
-//     let marker: GoogleMapsMarker;
-//     let map: GoogleMap;
-//     let position: google.maps.LatLng;
+        marker = new GoogleMapsMarker(api, new MockGoogleMap());
+    });
 
-//     beforeEach(async () =>
-//     {
-//         ({ api, spies: { runOutsideAngular } } = await configureGoogleMapsTestingModule());
-
-//         position = new google.maps.LatLng(11, 11);
-
-//         map = new GoogleMap(api, new ElementRef(elementStub));
-//         marker = new GoogleMapsMarker(api, map, { position });
-//     });
-
-//     describe('basically', () =>
-//     {
-//         it('should create an instance', () => expect(marker).toBeTruthy());
-//     });
-// });
+    describe('basically', () =>
+    {
+        it('should create an instance', () => expect(marker).toBeTruthy());
+    });
+});
