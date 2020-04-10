87bf46f8b0d96d3fac317311742c172699d81a64
Shy Agam <us@bespunky.io>
2020-04-10 13:09:41 +0300

    ➕ Exposed polygons as public api

--- projects/bespunky/angular-google-maps/src/public-api.ts
+++ projects/bespunky/angular-google-maps/src/public-api.ts
@@ -28,6 +28,10 @@ export * from './lib/google-map/types/zoom-level.enum';
 export * from './lib/overlays/marker/google-maps-marker';
 export * from './lib/overlays/marker/directive/google-maps-marker.directive';
 
+// Polygons
+export * from './lib/overlays/polygon/google-maps-polygon';
+export * from './lib/overlays/polygon/directive/google-maps-polygon.directive';
+
 // Data Layer
 export * from './lib/overlays/data/google-maps-data';
 export * from './lib/overlays/data/directive/google-maps-data.directive';
