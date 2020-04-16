58866bd75ff9c67211562997cd15a4ce96e09512
Shy Agam <us@bespunky.io>
2020-04-16 14:59:46 +0300

    ✅ Added tests for quick feature geometry setters

--- projects/bespunky/angular-google-maps/src/lib/overlays/data/feature/google-maps-feature.spec.ts
+++ projects/bespunky/angular-google-maps/src/lib/overlays/data/feature/google-maps-feature.spec.ts
@@ -7,29 +7,62 @@ import { MockGoogleMapsData } from '../testing/mock-google-maps-data.spec';
 
 describe('GoogleMapsFeature', () =>
 {
-    let api: GoogleMapsApiService;
-    let feature: GoogleMapsFeature;
-    let map: MockGoogleMap;
-    let data: MockGoogleMapsData;
+    let api              : GoogleMapsApiService;
+    let runOutsideAngular: jasmine.Spy;
+    let feature          : GoogleMapsFeature;
+    let map              : MockGoogleMap;
+    let data             : MockGoogleMapsData;
 
     beforeEach(async () =>
     {
-        ({ api } = await configureGoogleMapsTestingModule());
+        ({ api, spies: { runOutsideAngular } } = await configureGoogleMapsTestingModule());
 
-        map = new MockGoogleMap();
-        data = new MockGoogleMapsData(map);
+        map     = new MockGoogleMap();
+        data    = new MockGoogleMapsData(map);
         feature = new GoogleMapsFeature(api, data, { geometry: new google.maps.Data.Point({ lat: 2, lng: 2 }) });
+
+        runOutsideAngular.calls.reset();
     });
 
-    it('should create an instance', () => expect(feature).toBeTruthy());
-
-    it('should return a GeoJson object representing the feature when calling `toGeoJson()`', async () =>
+    describe('basically', () =>
     {
-        const geoJson = await feature.toGeoJson();
+        it('should create an instance', () => expect(feature).toBeTruthy());
 
-        expect(typeof geoJson).toBe('object');
-        expect(geoJson.type).toBe('Feature');
-        expect(geoJson?.geometry?.type).toBe('Point');
-        expect(geoJson?.geometry?.coordinates).toEqual([2, 2]);
+        it('should return a GeoJson object representing the feature when calling `toGeoJson()`', async () =>
+        {
+            const geoJson = await feature.toGeoJson();
+
+            expect(typeof geoJson).toBe('object');
+            expect(geoJson.type).toBe('Feature');
+            expect(geoJson?.geometry?.type).toBe('Point');
+            expect(geoJson?.geometry?.coordinates).toEqual([2, 2]);
+        });
+    });
+
+    describe('quick feature geometry', () =>
+    {
+        it('should create and set a point geometry outside angular when calling `setMarker()`', async () =>
+        {
+            feature.setMarker([1, 0]);
+
+            expect(runOutsideAngular).toHaveBeenCalledTimes(1);
+
+            const geometry = (await feature.toGeoJson()).geometry;
+
+            expect(geometry.type).toBe('Point');
+            expect(geometry.coordinates).toEqual([0, 1]);
+        });
+
+        it('should create and set a polygon geometry outside angular when calling `setPolygon()`', async () =>
+        {
+            feature.setPolygon([[0, 0], [0, 1], [1, 0], [1, 1]]);
+
+            expect(runOutsideAngular).toHaveBeenCalledTimes(1);
+
+            const geometry = (await feature.toGeoJson()).geometry;
+
+            expect(geometry.type).toBe('Polygon');
+            expect(geometry.coordinates).toEqual([[[0, 0], [1, 0], [0, 1], [1, 1], [0, 0]]]);
+        });
     });
 });
