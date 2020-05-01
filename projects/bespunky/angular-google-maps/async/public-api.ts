d285d0a59cdf02ed6d38c75bb08002da25fd9ed6
Shy Agam <us@bespunky.io>
2020-05-01 20:58:56 +0300

    🔧 Configured public api for async package

--- projects/bespunky/angular-google-maps/async/public-api.ts
+++ projects/bespunky/angular-google-maps/async/public-api.ts
@@ -1,9 +1,6 @@
+export * from './google-maps.module';
 
-export * from './async.module';
+export * from './services/google-maps-config';
+export * from './services/lazy-google-maps-api-loader';
 
-export * from './LazyLoader/lazy-loader.service';
-export * from './LazyLoader/script-load-options';
-export * from './LazyLoader/style-load-options';
-export * from './LazyLoader/lazy-loaded-file';
-
-export * from './utils/promiseLater';
+export * from './directives/safe.directive';
