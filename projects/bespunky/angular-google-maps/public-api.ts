bc334abcf8107f3fe45dbc8e368624960461f4e3
Shy Agam <us@bespunky.io>
2020-05-01 21:23:40 +0300

    🔧 Configured public api for library package

--- projects/bespunky/angular-google-maps/public-api.ts
+++ projects/bespunky/angular-google-maps/public-api.ts
@@ -1,23 +1,6 @@
 /*
  * Public API Surface of angular-google-maps
  */
-// Map
-export * from './lib/google-map/google-map';
-export * from './lib/google-map/component/google-map.component';
-export * from './lib/google-map/types/zoom-level.enum';
-
-// Markers
-export * from './lib/overlays/marker/google-maps-marker';
-export * from './lib/overlays/marker/directive/google-maps-marker.directive';
-
-// Polygons
-export * from './lib/overlays/polygon/google-maps-polygon';
-export * from './lib/overlays/polygon/directive/google-maps-polygon.directive';
-
-// Data Layer
-export * from './lib/overlays/data/google-maps-data';
-export * from './lib/overlays/data/directive/google-maps-data.directive';
-
-// Data Layer Feature
-export * from './lib/overlays/data/feature/google-maps-feature';
-export * from './lib/overlays/data/feature/directive/google-maps-feature.directive';
+export * from '@bespunky/angular-google-maps/core';
+export * from '@bespunky/angular-google-maps/async';
+export * from '@bespunky/angular-google-maps/overlays';
