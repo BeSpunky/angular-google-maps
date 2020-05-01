703f78a42efd35dd88607cda76620595d66b8808
Shy Agam <us@bespunky.io>
2020-05-01 20:57:54 +0300

    🔧 Configured public api for internal package

--- projects/bespunky/angular-google-maps/_internal/public-api.ts
+++ projects/bespunky/angular-google-maps/_internal/public-api.ts
@@ -1,9 +1,1 @@
-
-export * from './async.module';
-
-export * from './LazyLoader/lazy-loader.service';
-export * from './LazyLoader/script-load-options';
-export * from './LazyLoader/style-load-options';
-export * from './LazyLoader/lazy-loaded-file';
-
-export * from './utils/promiseLater';
+export * from './create-and-append-map-element';
