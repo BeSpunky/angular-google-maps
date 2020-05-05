1f6ebca481744dbceac75006904dccb546e01aaf
Shy Agam <us@bespunky.io>
2020-05-05 12:28:26 +0300

    ➕ Added overlays module to demo app

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { GoogleMapsModule } from '@bespunky/angular-google-maps/async';
+import { GoogleMapsOverlaysModule } from '../../../bespunky/angular-google-maps/overlays';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
             apiUrl: {
                 key: 'AIzaSyD2W_Cuuh-njEbh709te-kmcttVUlGoYeA'
             },
-        })
+        }),
+        GoogleMapsOverlaysModule
     ],
     bootstrap: [AppComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
