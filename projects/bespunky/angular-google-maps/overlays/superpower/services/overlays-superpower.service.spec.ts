e550efb6dba0777a8ef2a836d69dab7075542ff5
Shy Agam <us@bespunky.io>
2020-05-07 11:41:25 +0300

    ✅ Adapted overlays superpower tests

--- projects/bespunky/angular-google-maps/overlays/superpower/services/overlays-superpower.service.spec.ts
+++ projects/bespunky/angular-google-maps/overlays/superpower/services/overlays-superpower.service.spec.ts
@@ -1,74 +1,84 @@
-// import { ElementRef } from '@angular/core';
+import { TestBed    } from '@angular/core/testing';
+import { ElementRef } from '@angular/core';
 
-// import { configureGoogleMapsTestingModule, expectPositionEquals } from '@bespunky/angular-google-maps/testing';
-// import { GoogleMapsApiService, GoogleMap, Defaults, FlatCoord   } from '@bespunky/angular-google-maps/core';
-// import { GoogleMapsMarker, GoogleMapsPolygon, GoogleMapsData    } from '@bespunky/angular-google-maps/overlays';
+import { configureGoogleMapsTestingModule, expectPositionEquals                                              } from '@bespunky/angular-google-maps/testing';
+import { GoogleMapsApiService, GoogleMap, Defaults, FlatCoord, SuperpowersService                            } from '@bespunky/angular-google-maps/core';
+import { GoogleMapsMarker, GoogleMapsPolygon, GoogleMapsData, OverlaysSuperpowerProvider, OverlaysSuperpower } from '@bespunky/angular-google-maps/overlays';
 
-// const elementStub: any = document.createElement('div');
+const elementStub: any = document.createElement('div');
 
-// describe('OverlaysSuperpower', () =>
-// {
-//     let map              : GoogleMap;
-//     let api              : GoogleMapsApiService;
-//     let mapElement       : ElementRef;
-//     let runOutsideAngular: jasmine.Spy;
+describe('OverlaysSuperpower', () =>
+{
+    let superpowers      : SuperpowersService;
+    let power            : OverlaysSuperpower;
+    let map              : GoogleMap;
+    let api              : GoogleMapsApiService;
+    let mapElement       : ElementRef;
+    let runOutsideAngular: jasmine.Spy;
 
-//     beforeEach(async () =>
-//     {
-//         ({ api, spies: { runOutsideAngular } } = await configureGoogleMapsTestingModule());
+    beforeEach(async () =>
+    {
+        ({ api, spies: { runOutsideAngular } } = await configureGoogleMapsTestingModule({
+            customize: def => def.providers = [
+                SuperpowersService,
+                OverlaysSuperpowerProvider
+            ]
+        }));
 
-//         mapElement = new ElementRef(elementStub);
-//         map        = new GoogleMap(api, mapElement);
-//     });
+        superpowers = TestBed.inject(SuperpowersService);
+        mapElement  = new ElementRef(elementStub);
+        map         = new GoogleMap(superpowers, api, mapElement);
+        power       = map.superpowers.use(OverlaysSuperpower);
+    });
 
-//     it('should create a marker outside angular when calling `createMarker()`', () => 
-//     {            
-//         const marker = map.createMarker(Defaults.Center);
+    it('should create a marker outside angular when calling `createMarker()`', () => 
+    {            
+        const marker = power.createMarker(Defaults.Center);
         
-//         // Overlay creation ends up in more than one calls to runOutsideAngular().
-//         // In order to avoid changing the test when changing implementations, call count is avoided and this is used instead.
-//         expect(runOutsideAngular.calls.all().some(call => call.returnValue instanceof GoogleMapsMarker)).toBeTruthy();
+        // Overlay creation ends up in more than one calls to runOutsideAngular().
+        // In order to avoid changing the test when changing implementations, call count is avoided and this is used instead.
+        expect(runOutsideAngular.calls.all().some(call => call.returnValue instanceof GoogleMapsMarker)).toBeTruthy();
 
-//         expect(marker instanceof GoogleMapsMarker).toBeTruthy();
-//         expectPositionEquals(marker.getPosition(), Defaults.Center);
-//     });
+        expect(marker instanceof GoogleMapsMarker).toBeTruthy();
+        expectPositionEquals(marker.getPosition(), Defaults.Center);
+    });
 
-//     it('should create a polygon outside angular when calling `createPolygon()`', () => 
-//     {            
-//         const flatPath = [[10, 20], [20, 30], [30, 40]] as FlatCoord[];
-//         const polygon = map.createPolygon(flatPath);
+    it('should create a polygon outside angular when calling `createPolygon()`', () => 
+    {            
+        const flatPath = [[10, 20], [20, 30], [30, 40]] as FlatCoord[];
+        const polygon = power.createPolygon(flatPath);
         
-//         // Overlay creation ends up in more than one calls to runOutsideAngular().
-//         // In order to avoid changing the test when changing implementations, call count is avoided and this is used instead.
-//         expect(runOutsideAngular.calls.all().some(call => call.returnValue instanceof GoogleMapsPolygon)).toBeTruthy();
+        // Overlay creation ends up in more than one calls to runOutsideAngular().
+        // In order to avoid changing the test when changing implementations, call count is avoided and this is used instead.
+        expect(runOutsideAngular.calls.all().some(call => call.returnValue instanceof GoogleMapsPolygon)).toBeTruthy();
 
-//         expect(polygon instanceof GoogleMapsPolygon).toBeTruthy();
-//         expect(polygon.getPath()).toEqual(api.geometry.toLiteralMultiPath(flatPath));
-//     });
+        expect(polygon instanceof GoogleMapsPolygon).toBeTruthy();
+        expect(polygon.getPath()).toEqual(api.geometry.toLiteralMultiPath(flatPath));
+    });
     
-//     it('should create a data layer outside angular when calling `createDataLayer()`', () => 
-//     {
-//         const data = map.createDataLayer({ style: { title: 'awesome' } });
+    it('should create a data layer outside angular when calling `createDataLayer()`', () => 
+    {
+        const data = power.createDataLayer({ style: { title: 'awesome' } });
 
-//         // Overlay creation ends up in more than one calls to runOutsideAngular().
-//         // In order to avoid changing the test when changing implementations, call count is avoided and this is used instead.
-//         expect(runOutsideAngular.calls.all().some(call => call.returnValue instanceof GoogleMapsData)).toBeTruthy();
+        // Overlay creation ends up in more than one calls to runOutsideAngular().
+        // In order to avoid changing the test when changing implementations, call count is avoided and this is used instead.
+        expect(runOutsideAngular.calls.all().some(call => call.returnValue instanceof GoogleMapsData)).toBeTruthy();
 
-//         expect(data instanceof GoogleMapsData).toBeTruthy();
-//         expect((data.getStyle() as any).title).toBe('awesome');
-//     });
+        expect(data instanceof GoogleMapsData).toBeTruthy();
+        expect((data.getStyle() as any).title).toBe('awesome');
+    });
 
-//     it('should remove an overlay outside angular when calling `removeOverlay()`', () =>
-//     {
-//         const marker = map.createMarker({ lat: 20, lng: 20 });
+    it('should remove an overlay outside angular when calling `removeOverlay()`', () =>
+    {
+        const marker = power.createMarker({ lat: 20, lng: 20 });
 
-//         expect(map.overlays.markers.includes(marker)).toBeTruthy();
+        expect(power.tracker.markers.includes(marker)).toBeTruthy();
 
-//         runOutsideAngular.calls.reset();
+        runOutsideAngular.calls.reset();
 
-//         map.removeOverlay(marker);
+        power.removeOverlay(marker);
 
-//         expect(runOutsideAngular.calls.count()).toBeGreaterThan(0);
-//         expect(map.overlays.markers.includes(marker)).toBeFalsy();
-//     });
-// });
+        expect(runOutsideAngular.calls.count()).toBeGreaterThan(0);
+        expect(power.tracker.markers.includes(marker)).toBeFalsy();
+    });
+});
