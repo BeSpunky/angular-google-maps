7647d1b27a4fa6e21d68eccd16235c8f44224ea0
Shy Agam <us@bespunky.io>
2020-05-19 11:34:40 +0300

    ✅ Adapted superpowers service

--- projects/bespunky/angular-google-maps/core/modules/map/superpowers/superpowers.service.spec.ts
+++ projects/bespunky/angular-google-maps/core/modules/map/superpowers/superpowers.service.spec.ts
@@ -1,53 +1,73 @@
-import { TestBed } from '@angular/core/testing';
-import { Type    } from '@angular/core';
+import { TestBed          } from '@angular/core/testing';
+import { Type, Injectable } from '@angular/core';
 
 import { configureGoogleMapsTestingModule                                       } from '@bespunky/angular-google-maps/testing';
 import { MockSuperpower1, MockSuperpower2, MockGoogleMap                        } from '@bespunky/angular-google-maps/core/testing';
-import { SuperpowersService, createSuperpowerProvider, Superpowers, ISuperpower } from '@bespunky/angular-google-maps/core';
+import { SuperpowersService, SuperpowersChargerService, ISuperpower, Superpower } from '@bespunky/angular-google-maps/core';
 
 describe('SuperpowersService', () =>
 {
+    let charger    : SuperpowersChargerService;
     let superpowers: SuperpowersService;
 
     beforeEach(async () =>
     {
         await configureGoogleMapsTestingModule({
-            customize: def => def.providers = [
-                // Add 2 superpowers for testing
-                createSuperpowerProvider(MockSuperpower1),
-                createSuperpowerProvider(MockSuperpower2),
-                SuperpowersService
-            ]
+            customize: def => def.providers = [SuperpowersService]
         });
 
+        charger     = TestBed.inject(SuperpowersChargerService);
         superpowers = TestBed.inject(SuperpowersService);
+
+        charger.charge(MockSuperpower1);
+        charger.charge(MockSuperpower2);
     });
 
     it('should be created', () => expect(superpowers).toBeTruthy());
 
-    it('should initialize correctly without superpower providers', () =>
-    {
-        expect(new SuperpowersService(null)).toBeTruthy();
-        expect(new SuperpowersService(undefined)).toBeTruthy();
-    });
+    it('should return the number of charged superpowers', () => expect(superpowers.count).toBe(2));
 
     it('should attach all superpowers to a map when calling `attachToMap()`', () =>
     {
-        const powers = TestBed.inject(Superpowers);
-
-        expect(powers.length).toBe(2);
         // Expect all powers to be detached
-        expect(powers.every(power => !power.map)).toBeTruthy();
+        expect(superpowers.use(MockSuperpower1).map).toBeUndefined();
+        expect(superpowers.use(MockSuperpower2).map).toBeUndefined();
+        
+        const map = new MockGoogleMap();
+        
+        superpowers.attachToMap(map);
+        
+        // Expect all powers to be attached to the new map
+        expect(superpowers.use(MockSuperpower1).map).toBe(map);
+        expect(superpowers.use(MockSuperpower2).map).toBe(map);
+    });
 
+    it('should not load a power twice', () =>
+    {
+        expect(superpowers.count).toBe(2);
+
+        charger.charge(MockSuperpower2);
+
+        expect(superpowers.count).toBe(2);
+    });
+    
+    it('should detect, load and attach new powers charged after init', () =>
+    {
         const map = new MockGoogleMap();
 
         superpowers.attachToMap(map);
 
-        // Expect all powers to be attached to the new map
-        expect(powers.every(power => power.map === map)).toBeTruthy();
+        charger.charge(MockSuperpower3);
+
+        expect(() => superpowers.use(MockSuperpower3)).not.toThrow();
+        expect(superpowers.use(MockSuperpower3)).toBeInstanceOf(MockSuperpower3);
+        expect(superpowers.use(MockSuperpower3).map).toBe(map);
     });
 
     it('should retrieve a superpower when calling `use()`', () => expect(superpowers.use(MockSuperpower1) instanceof MockSuperpower1).toBeTruthy());
 
-    it('should return `undefined` when calling `use()` with a non-registered superpower', () => expect(superpowers.use({} as Type<ISuperpower>)).toBeUndefined());
+    it('should throw when calling `use()` with a non-registered superpower', () => expect(() => superpowers.use({} as Type<ISuperpower>)).toThrowError(/hasn't been registered/));
 });
+
+@Injectable({ providedIn: 'root' })
+class MockSuperpower3 extends Superpower { }
