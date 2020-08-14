91b8f841be13ef7d25886e68426ba0229f645e55
Shy Agam <us@bespunky.io>
2020-08-14 17:29:05 +0300

    ➖ Removed src folder

--- projects/bespunky/angular-google-maps/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of angular-google-maps
- */
-
-export * from './lib/angular-google-maps.service';
-export * from './lib/angular-google-maps.component';
-export * from './lib/angular-google-maps.module';
