c1e776bcfc2e2fa321407490ccc254e01071f2ed
Shy Agam <us@bespunky.io>
2020-08-16 12:51:36 +0300

    📦 Installed angular material

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExampleListComponent } from './components/example-list/example-list.component';
 import { ExamplesComponent } from './components/examples/examples.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ExamplesComponent } from './components/examples/examples.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
