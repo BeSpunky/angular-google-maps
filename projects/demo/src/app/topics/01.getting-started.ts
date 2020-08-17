288b8aeac80a86b5f23ab36434613818f291dc7b
Shy Agam <us@bespunky.io>
2020-08-17 22:49:45 +0300

    ✍ Updated getting started examples

--- projects/demo/src/app/topics/01.getting-started.ts
+++ projects/demo/src/app/topics/01.getting-started.ts
@@ -6,13 +6,22 @@ export const GettingStartedExamples: Example[] = [
         title      : 'Plug & Play',
         description: 'Have the library import Google Maps API for you and add a simple map.',
         notes      : 'You\'ll need to have you\'r own key to Google Maps API. See example for link.',
+        icon       : 'power',
         embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-plug-n-play?embed=1&file=src/app/app.module.ts'
     },
     {
-        title      : 'Manual Loading',
+        title      : 'Custom Loader',
         description: 'Implement your own mechanism for loading Google Maps API.',
         notes      : 'You\'ll need to have you\'r own key to Google Maps API. See example for link.',
-        embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-simple?embed=1&file=src/app/app.module.ts&theme=dark'
+        icon       : 'build',
+        embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-custom-loader?embed=1&file=src/app/app.module.ts'
+    },
+    {
+        title      : 'Manual Loading',
+        description: 'Manually add a <script> tag to your <head> element to ensure maps API completely loads before the your app does.',
+        notes      : 'You\'ll need to have you\'r own key to Google Maps API. See example for link.',
+        icon       : 'engineering',
+        embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-manual-loading?embed=1&file=src/app/app.module.ts'
     }
 ];
 
