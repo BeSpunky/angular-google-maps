28481e9e4623086b5f337bc0db5d0f459d57a519
Shy Agam <us@bespunky.io>
2020-08-18 00:56:17 +0300

    📃 Updated the map examples

--- projects/demo/src/app/topics/02.the-map.ts
+++ projects/demo/src/app/topics/02.the-map.ts
@@ -3,18 +3,21 @@ import { Topic   } from '../types/topic';
 
 export const TheMapExamples: Example[] = [
     {
-        title      : 'Map Events',
-        description: 'Bind event handlers and react to user interactions with the map.',
-        embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-simple?embed=1&file=src/app/app.module.ts&theme=dark'
-    },
-    {
         title      : 'Map Options',
         description: 'Set options to your map (e.g. center, zoom, etc.)',
-        embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-simple?embed=1&file=src/app/app.module.ts&theme=dark'
+        icon       : 'tune',
+        embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-map-options?embed=1&file=src/app/map/map.component.html'
+    },
+    {
+        title      : 'Map Events',
+        description: 'Bind event handlers and react to user interactions with the map.',
+        icon       : 'flash_on',
+        embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-map-events?embed=1&file=src/app/map/map.component.html'
     },
     {
         title      : 'Map Wrapper',
         description: 'Get to know the map object and unlock the full potential of your map.',
+        icon       : 'widgets',
         embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-simple?embed=1&file=src/app/app.module.ts&theme=dark'
     }
 ];
