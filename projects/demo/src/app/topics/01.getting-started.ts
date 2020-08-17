38cce0218274db709e4e90a45b89e9538cfd393d
Shy Agam <us@bespunky.io>
2020-08-17 13:51:39 +0300

    📝 Updated getting started example

--- projects/demo/src/app/topics/01.getting-started.ts
+++ projects/demo/src/app/topics/01.getting-started.ts
@@ -3,15 +3,15 @@ import { Topic   } from '../types/topic';
 
 export const GettingStartedExamples: Example[] = [
     {
-        title      : 'Plug & Play (Simple Map)',
+        title      : 'Plug & Play',
         description: 'Have the library import Google Maps API for you and add a simple map.',
-        notes      : 'You\'ll need to have you\'r own key to Google Maps API. See README.md for more info.',
-        embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-simple?embed=1&file=src/app/app.module.ts&theme=dark'
+        notes      : 'You\'ll need to have you\'r own key to Google Maps API. See example for link.',
+        embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-plug-n-play?embed=1&file=src/app/app.module.ts'
     },
     {
         title      : 'Manual Loading',
         description: 'Implement your own mechanism for loading Google Maps API.',
-        notes      : 'You\'ll need to have you\'r own key to Google Maps API. See README.md for more info.',
+        notes      : 'You\'ll need to have you\'r own key to Google Maps API. See example for link.',
         embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-simple?embed=1&file=src/app/app.module.ts&theme=dark'
     }
 ];
