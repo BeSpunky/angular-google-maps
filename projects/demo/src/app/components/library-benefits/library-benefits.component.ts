3885504d782bb376ff2d32452f8eccc28ea07987
Shy Agam <us@bespunky.io>
2020-08-17 12:00:40 +0300

    ✍ Improved benefit description

--- projects/demo/src/app/components/library-benefits/library-benefits.component.ts
+++ projects/demo/src/app/components/library-benefits/library-benefits.component.ts
@@ -14,7 +14,7 @@ export class LibraryBenefitsComponent
         { title: 'Tree-shakable',                      emoji: '🌳', description: 'Any library module you don\'t use in your app will be excluded from your final bundle.' },
         { title: 'Angular Universal ready',            emoji: '🌎', description: 'Developed with SSR in mind. Usage stays the same, and your app is safe.' },
         { title: 'Extendable',                         emoji: '🧩', description: 'Superpowers which allows your to create your own pluggable map extensions and share them with the world!' },
-        { title: 'Testable',                           emoji: '🧪', description: 'Abstraction layer and testing modules facilitate mocking and testing.' },
+        { title: 'Testable',                           emoji: '🧪', description: 'Abstraction layer and testing suite facilitate mocking and testing.' },
         { title: 'Scalable',                           emoji: '↗' , description: 'Layered and abstracted infrastructure allows for easier development of new features and supporting more of the native API.' },
         { title: 'Magical automated native wrapping',  emoji: '✨', description: 'Interaction with native object is done through a set of automating tools which you can also use to create your own components!' },
         { title: 'Open-source!',                       emoji: '🤩', description: 'Learn how it works! Contribute! Fork it! Make it your own!' }
