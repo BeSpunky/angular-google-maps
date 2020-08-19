e5da02efb8c6072b0880443adf7bcf96f595cae2
Shy Agam <us@bespunky.io>
2020-08-19 19:28:59 +0300

    ➕ Implemented flexible types for fitBounds and panToBounds

--- projects/bespunky/angular-google-maps/core/modules/map/i-google-map.ts
+++ projects/bespunky/angular-google-maps/core/modules/map/i-google-map.ts
@@ -1,5 +1,5 @@
 import { IGoogleMapsNativeObjectEmittingWrapper } from '../../abstraction/base/i-google-maps-native-object-emitting-wrapper';
-import { Coord                                  } from '../../abstraction/types/geometry.type';
+import { Coord, BoundsLike                      } from '../../abstraction/types/geometry.type';
 import { ISuperpowers                           } from './superpowers/i-superpowers';
 
 export interface IGoogleMap extends IGoogleMapsNativeObjectEmittingWrapper<google.maps.Map>
@@ -7,4 +7,18 @@ export interface IGoogleMap extends IGoogleMapsNativeObjectEmittingWrapper<googl
     readonly superpowers: ISuperpowers;
 
     setCenter(center: Coord): void;
+
+    fitBounds(...elements: BoundsLike[]): void;
+    
+    fitBoundsWithPadding(padding: number | google.maps.Padding, ...elements: BoundsLike[]): void;
+    
+    panTo(position: Coord): void;
+    
+    panToBounds(...elements: BoundsLike[]): void;
+    
+    panToBoundsWithPadding(padding: number | google.maps.Padding, ...elements: BoundsLike[]): void;
+    
+    getMapType(): string | google.maps.MapTypeId;
+    
+    setMapType(type: string | google.maps.MapTypeId): void;
 }
