e9d7612c86e3c450819f01666a8ef23f8b271602
Shy Agam <us@bespunky.io>
2020-08-20 12:08:45 +0300

    🐛 Fixed type-guard failure on null and undefined

--- projects/bespunky/angular-google-maps/core/abstraction/type-guards/geometry-type-guards.ts
+++ projects/bespunky/angular-google-maps/core/abstraction/type-guards/geometry-type-guards.ts
@@ -2,7 +2,7 @@ import { IBounds } from '../base/i-bounds';
 
 export function isBoundsLiteral(object: any): object is google.maps.LatLngBoundsLiteral
 {
-    return object && object.north && object.south && object.east && object.west;
+    return !!(object && object.north && object.south && object.east && object.west);
 }
 
 export function hasBounds(object: any): object is IBounds
