06568ca39a1397e46a96240437147140712b0950
Shy Agam <us@bespunky.io>
2020-08-24 15:19:26 +0300

    ➕ Added overlays superpower examples

--- projects/demo/src/app/topics/07.overlays-superpower.ts
+++ projects/demo/src/app/topics/07.overlays-superpower.ts
@@ -6,29 +6,25 @@ export const OverlaysSuperpowerExamples: Example[] = [
         title      : 'Markers',
         description: 'Add markers (pins) to the map.',
         icon       : 'location_on',
-        embedUrl   : '',
-        soon       : true
+        embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-practices-overlays-superpower?embed=1&file=src/app/continents-map/components/continent-overlays/continent-overlays.component.html'
     },
     {
         title      : 'Polygons',
         description: 'Draw polygons on the map.',
         icon       : 'change_history',
-        embedUrl   : '',
-        soon       : true
+        embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-practices-overlays-superpower?embed=1&file=src/app/continents-map/components/continent-overlays/continent-overlays.component.html'
     },
     {
         title      : 'Data Layer',
         description: 'Load different overlays straight from a GeoJson object, or manually add features.',
         icon       : 'category',
-        embedUrl   : '',
-        soon       : true
+        embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-practices-overlays-superpower?embed=1&file=src/app/continents-data-layer-map/continents-data-layer-map.component.html'
     },
     {
         title      : 'Overlay Tracking',
         description: 'Use the superpower\'s tracker to get a hold of all overlays you have placed on the map.',
         icon       : 'track_changes',
-        embedUrl   : '',
-        soon       : true
+        embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-practices-overlays-superpower?embed=1&file=src/app/continents-map/continents-map.component.ts'
     }
 ];
 
