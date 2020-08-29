3c4110c63e9c7522dd810f8bc5051d4639f9821b
Shy Agam <us@bespunky.io>
2020-08-29 18:40:38 +0300

    ✅ Added tests for info window directive

--- projects/bespunky/angular-google-maps/overlays/modules/info-window/directive/google-maps-info-window.directive.spec.ts
+++ projects/bespunky/angular-google-maps/overlays/modules/info-window/directive/google-maps-info-window.directive.spec.ts
@@ -1,9 +1,11 @@
 import { ComponentFixture } from '@angular/core/testing';
 import { Component        } from '@angular/core';
 
-import { configureGoogleMapsTestingModule                                        } from '@bespunky/angular-google-maps/testing';
-import { createLifecycleTestingHostComponentTemplate, LifecycleComponentTestHost } from '@bespunky/angular-google-maps/core/testing';
-import { GoogleMapsInfoWindowDirective, GoogleMapsOverlaysModule                     } from '@bespunky/angular-google-maps/overlays';
+import { configureGoogleMapsTestingModule                                              } from '@bespunky/angular-google-maps/testing';
+import { createLifecycleTestingHostComponentTemplate, LifecycleComponentTestHost       } from '@bespunky/angular-google-maps/core/testing';
+import { MockMouseEventEmitter                                                         } from '@bespunky/angular-google-maps/overlays/testing';
+import { GoogleMapsComponentApiService                                                 } from '@bespunky/angular-google-maps/core';
+import { GoogleMapsInfoWindowDirective, GoogleMapsOverlaysModule, GoogleMapsInfoWindow } from '@bespunky/angular-google-maps/overlays';
 
 /**
  * -- NOTE --
@@ -17,24 +19,87 @@ describe('GoogleMapsInfoWindowDirective', () =>
 {
     let hostFixture  : ComponentFixture<TestHostComponent>;
     let hostComponent: TestHostComponent;
+    let componentApi: GoogleMapsComponentApiService;
     let directive    : GoogleMapsInfoWindowDirective;
+    let infoWindow   : GoogleMapsInfoWindow;
 
     beforeEach(async () =>
     {
-        ({ fixture: hostFixture, component: hostComponent } = await configureGoogleMapsTestingModule({
+        ({ fixture: hostFixture, component: hostComponent, componentApi } = await configureGoogleMapsTestingModule({
             componentType: TestHostComponent,
-            customize    : def => def.imports.push(GoogleMapsOverlaysModule)
+            customize: def => def.imports.push(GoogleMapsOverlaysModule)
         }));
         
         hostFixture.detectChanges();
 
-        directive = hostComponent.testedComponent as GoogleMapsInfoWindowDirective;
+        directive  = hostComponent.testedComponent as GoogleMapsInfoWindowDirective;
+        infoWindow = directive.wrapper as GoogleMapsInfoWindow;
     });
 
+    function getContent(): HTMLElement
+    {
+        const content = infoWindow.getContent();
+        
+        if (typeof content === 'string')
+        {
+            const element = document.createElement('div');
+            element.innerHTML = content;
+
+            return element.firstChild as HTMLElement;
+        }
+
+        return content as HTMLElement;
+    }
+
     it('should create an instance', () => expect(directive).toBeTruthy());
+
+    it('should set a parent div.google-maps-info-window in the info window\'s content', () =>
+    {
+        const content = getContent();
+
+        expect(content.tagName).toBe('DIV');
+        expect(content.className).toBe('google-maps-info-window');
+    });
+
+    it('should set its child nodes as content for the info window', () =>
+    {
+        const content = getContent();
+
+        expect(content.children.length).toBe(1);
+        expect(content.firstChild.nodeName).toBe('DIV');
+    });
+
+    it('should observe child nodes and keep the content up to date', () =>
+    {
+        hostComponent.values.push(hostComponent.values.length);
+        hostComponent.values.push(hostComponent.values.length);
+        hostComponent.values.push(hostComponent.values.length);
+
+        hostFixture.detectChanges();
+
+        const content = getContent();
+
+        expect(content.children.length).toBe(hostComponent.values.length);
+    });
+
+    it('should detach the emmitter from the info window when destroyed', () =>
+    {
+        infoWindow.setAttachedTo(new MockMouseEventEmitter(componentApi, infoWindow));
+
+        directive.ngOnDestroy();
+
+        expect(infoWindow.getAttachedTo()).toBeFalsy();
+    });
 });
 
 @Component({
-    template: createLifecycleTestingHostComponentTemplate('<bs-google-maps-info-window [position]="center" #testedComponent></bs-google-maps-info-window>')
+    template: createLifecycleTestingHostComponentTemplate(
+        `<bs-google-maps-info-window [position]="center" #testedComponent="infoWindow">
+            <div *ngFor="let n of values">{{n}}</div>
+        </bs-google-maps-info-window>`
+    )
 })
-class TestHostComponent extends LifecycleComponentTestHost { }
+class TestHostComponent extends LifecycleComponentTestHost
+{
+    public values: number[] = [1];
+}
