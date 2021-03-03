9468db0dc56932845e33ebe6e17b8d197523d266
Shy Agam <us@bespunky.io>
2021-03-03 22:07:51 +0200

    ♻ Improved flexible directions types definitions

--- projects/bespunky/angular-google-maps/directions/abstraction/types/types.ts
+++ projects/bespunky/angular-google-maps/directions/abstraction/types/types.ts
@@ -1,4 +1,14 @@
 import { BoundsLike, NativeCoord } from '@bespunky/angular-google-maps/core';
 
 export type NativeDirectionsPlace = string | NativeCoord | google.maps.Place;
-export type DirectionsPlace       = string | google.maps.Place | BoundsLike;
+export type DirectionsPlace       = NativeDirectionsPlace | DirectionsWaypoint | BoundsLike;
+
+export type NativeDirectionsWaypoint = google.maps.DirectionsWaypoint;
+export type DirectionsWaypoint       = NativeDirectionsWaypoint | Omit<NativeDirectionsWaypoint, 'location'> & {
+    /**
+    * The place this waypoint points to.
+    *
+    * @type {DirectionsPlace}
+    */
+    location?: DirectionsPlace
+};
