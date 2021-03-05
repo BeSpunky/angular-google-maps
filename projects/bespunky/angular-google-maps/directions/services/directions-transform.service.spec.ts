67a8019b751c9cff6293e2c12f80bb6a1c0abb90
Shy Agam <us@bespunky.io>
2021-03-06 00:40:41 +0200

    ✅ Added tests for the directions transform service

--- projects/bespunky/angular-google-maps/directions/services/directions-transform.service.spec.ts
+++ projects/bespunky/angular-google-maps/directions/services/directions-transform.service.spec.ts
@@ -1,16 +1,88 @@
+import { producePlaceSpecs, produceWaypointSpecs, produceNativePlaceSpecs, produceFlexiblePlaceSpecs, expectPlace, expectWaypoint, produceNativeWaypointSpecs, produceFlexibleWaypointSpecs } from '@bespunky/angular-google-maps/directions/testing';
+import { GeometryTransformService   } from '@bespunky/angular-google-maps/core';
+import { DirectionsTransformService } from '@bespunky/angular-google-maps/directions';
+import { configureGoogleMapsTestingModule } from '@bespunky/angular-google-maps/async/testing';
 import { TestBed } from '@angular/core/testing';
 
-import { DirectionsTransformService } from './directions-transform.service';
+describe('DirectionsTransformService', () =>
+{
+    let geometry  : GeometryTransformService;
+    let directions: DirectionsTransformService;
 
-describe('DirectionsTransformService', () => {
-  let service: DirectionsTransformService;
+    beforeAll(() =>
+    {
+        configureGoogleMapsTestingModule();
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(DirectionsTransformService);
-  });
+        geometry   = TestBed.inject(GeometryTransformService);
+        directions = TestBed.inject(DirectionsTransformService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    function testTypeGuardFalseAgainstStandardValues(guard: (value: any) => boolean)
+    {
+        expect(guard(null)).toBeFalsy();
+        expect(guard(undefined)).toBeFalsy();
+        expect(guard({})).toBeFalsy();
+        expect(guard([])).toBeFalsy();
+        expect(guard([[]])).toBeFalsy();
+        expect(guard(10)).toBeFalsy();
+    }
+
+    describe('Basically', () =>
+    {
+        it('should be created', () => expect(directions).toBeTruthy());
+    });
+
+    describe('toNativePlace', () => producePlaceSpecs('transform place to a native place', place =>
+    {
+        const native = directions.toNativePlace(place);
+
+        expect(directions.isNativePlace(native)).toBeTrue();
+        expectPlace(() => native, place);
+    }));
+    
+    describe('isNativePlace', () =>
+    {
+        it('should determine whether an object is a native place', () =>
+        {
+            testTypeGuardFalseAgainstStandardValues((value) => directions.isNativePlace(value));
+            
+            expect(directions.isNativePlace({ location: 123 })).toBeFalse();
+        });
+
+        produceNativePlaceSpecs  ('determine whether the object is a native place', place => expect(directions.isNativePlace(place)).toBeTruthy());
+        produceFlexiblePlaceSpecs('determine whether the object is a native place', place => expect(directions.isNativePlace(place)).toBeFalse());
+    });
+
+    describe('toNativeWaypoint', () => produceWaypointSpecs('transform waypoint to a native waypoint', waypoint =>
+    {
+        const native = directions.toNativeWaypoint(waypoint);
+
+        expect(directions.isNativeWaypoint(native)).toBeTrue();
+        expectWaypoint(() => native, waypoint);
+    }));
+
+    describe('isNativeWaypoint', () =>
+    {
+        it('should determine whether an object is a native waypoint', () =>
+        {
+            testTypeGuardFalseAgainstStandardValues((value) => directions.isNativeWaypoint(value));
+            
+            expect(directions.isNativeWaypoint({ location: 123 })).toBeFalse();
+        });
+
+        produceNativeWaypointSpecs  ('determine whether the object is a native waypoint', waypoint => expect(directions.isNativeWaypoint(waypoint)).toBeTrue());
+        produceFlexibleWaypointSpecs('determine whether the object is a native waypoint', waypoint => expect(directions.isNativeWaypoint(waypoint)).toBeFalse());
+    });
+
+    describe('isWaypoint', () =>
+    {
+        it('should determine whether an object is a waypoint', () =>
+        {
+            testTypeGuardFalseAgainstStandardValues((value) => directions.isWaypoint(value));
+            
+            expect(directions.isWaypoint({ location: 123 })).toBeFalse();
+        });
+
+        produceWaypointSpecs('determine whether the object is a waypoint', waypoint => expect(directions.isWaypoint(waypoint)).toBeTrue());
+    });
 });
