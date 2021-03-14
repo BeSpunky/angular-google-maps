9734315991a3aa45bc8b5527a38df6623b8fd0e6
Shy Agam <us@bespunky.io>
2021-03-14 14:34:21 +0200

    ✅ Added missing test to abstract native wrapper class

--- projects/bespunky/angular-google-maps/core/abstraction/base/google-maps-native-object-wrapper.spec.ts
+++ projects/bespunky/angular-google-maps/core/abstraction/base/google-maps-native-object-wrapper.spec.ts
@@ -10,26 +10,34 @@ describe('GoogleMapsNativeObjectWrapper (abstract)', () =>
     let mockNative       : MockNative;
     let mockWrapper      : GoogleMapsNativeObjectWrapperTest;
 
-    beforeEach(async () =>
+    describe('with a native', () =>
     {
-        ({ api, spies: { runOutsideAngular } } = await configureGoogleMapsTestingModule());
-
-        mockNative  = new MockNative();
-        mockWrapper = new GoogleMapsNativeObjectWrapperTest(api, mockNative);
+        beforeEach(async () =>
+        {
+            ({ api, spies: { runOutsideAngular } } = await configureGoogleMapsTestingModule());
+    
+            mockNative  = new MockNative();
+            mockWrapper = new GoogleMapsNativeObjectWrapperTest(api, mockNative);
+        });
+    
+        it('should create an instance when instantiated by a derived class', () => expect(mockWrapper).toBeTruthy());
+    
+        it('should instantiate and assign a new native object accessible through the `native` property', () => expect(mockWrapper.native).toBe(mockNative));
+    
+        it('should set the custom object when calling `setCustom()`', () =>
+        {
+            mockWrapper.setCustom(123);
+    
+            expect(mockWrapper.custom).toBe(123);
+        });
     });
 
-    it('should create an instance when instantiated by a derived class', () => expect(mockWrapper).toBeTruthy());
-
-    it('should instantiate and assign a new native object accessible through the `native` property', () => expect(mockWrapper.native).toBe(mockNative));
-
-    it('should set the custom object when calling `setCustom()`', () =>
+    describe('without a native', () =>
     {
-        mockWrapper.setCustom(123);
+        beforeEach(async () => ({ api } = await configureGoogleMapsTestingModule()));
 
-        expect(mockWrapper.custom).toBe(123);
-    });
+        it('should throw an error if no native object was defined', () => expect(() => new GoogleMapsNativeObjectWrapperTest(api, null)).toThrow());
+    });    
 });
 
-class GoogleMapsNativeObjectWrapperTest extends GoogleMapsNativeObjectWrapper<MockNative>
-{
-}
+class GoogleMapsNativeObjectWrapperTest extends GoogleMapsNativeObjectWrapper<MockNative> { }
