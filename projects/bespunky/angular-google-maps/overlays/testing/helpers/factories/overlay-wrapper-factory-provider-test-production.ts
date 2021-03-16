27ab322cdd732946013df705f51ecfaffab3fb65
Shy Agam <us@bespunky.io>
2021-03-16 22:03:01 +0200

    📃 Added missing docs to overlays testing suite for factories

--- projects/bespunky/angular-google-maps/overlays/testing/helpers/factories/overlay-wrapper-factory-provider-test-production.ts
+++ projects/bespunky/angular-google-maps/overlays/testing/helpers/factories/overlay-wrapper-factory-provider-test-production.ts
@@ -3,10 +3,15 @@ import { MockNativeDrawableOverlay                                  } from '../.
 import { setupOverlayWrapperFactoryProviderTest                     } from './setup/overlay-wrapper-factory-provider-test-setup';
 
 /**
- *
+ * Performs setup and spec production for an **overlay** wrapper factory providers using the default `setupOverlayWrapperFactoryProviderTest()` as a setup function.
+ * Also adds a `MockNativeDrawableOverlay` as the native injected into the wrapper.
+ * 
+ * How:
+ * Creates a testing section with the provider name using `describe()`, sets up the testing environment using the default setup function
+ * and produces native factory provider specs using the `produceWrapperFactoryProviderSpecs()` function.
  *
  * @export
- * @param {(Omit<WrapperFactoryProviderTest, 'setup' | 'mockNative'>)} testConfig
+ * @param {(Omit<WrapperFactoryProviderTest, 'setup' | 'mockNative'>)} testConfig The configuration for the test.
  */
 export function testOverlayWrapperFactoryProvider(testConfig: Omit<WrapperFactoryProviderTest, 'setup' | 'mockNative'>)
 {
--- projects/bespunky/angular-google-maps/overlays/testing/helpers/overlay-native-factory-provider-test-production.ts
+++
@@ -1,8 +0,0 @@
-import { NativeFactoryProviderTest, setupNativeFactoryProviderTest, testNativeFactoryProviderCore } from '@bespunky/angular-google-maps/core/testing';
-
-export function testOverlayNativeFactoryProvider(testConfig: Omit<NativeFactoryProviderTest, 'setup'>)
-{
-    const config = { ...testConfig, setup: setupNativeFactoryProviderTest };
-
-    testNativeFactoryProviderCore(config);
-}
