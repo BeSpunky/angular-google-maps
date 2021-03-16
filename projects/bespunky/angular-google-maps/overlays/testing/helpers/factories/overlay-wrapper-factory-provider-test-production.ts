c5ab9da5a483db108022ede6d79ef4be7cb16d90
Shy Agam <us@bespunky.io>
2021-03-16 22:00:34 +0200

    📂 Restructured helpers folder in overlays testing suite to accommodate core testing suite structure

--- projects/bespunky/angular-google-maps/overlays/testing/helpers/factories/overlay-wrapper-factory-provider-test-production.ts
+++ projects/bespunky/angular-google-maps/overlays/testing/helpers/factories/overlay-wrapper-factory-provider-test-production.ts
@@ -1,7 +1,13 @@
 import { WrapperFactoryProviderTest, testWrapperFactoryProviderCore } from '@bespunky/angular-google-maps/core/testing';
-import { MockNativeDrawableOverlay                                  } from '../mocks/mock-native-drawable-overlay';
-import { setupOverlayWrapperFactoryProviderTest                     } from './overlay-wrapper-factory-provider-test-setup';
+import { MockNativeDrawableOverlay                                  } from '../../mocks/mock-native-drawable-overlay';
+import { setupOverlayWrapperFactoryProviderTest                     } from './setup/overlay-wrapper-factory-provider-test-setup';
 
+/**
+ *
+ *
+ * @export
+ * @param {(Omit<WrapperFactoryProviderTest, 'setup' | 'mockNative'>)} testConfig
+ */
 export function testOverlayWrapperFactoryProvider(testConfig: Omit<WrapperFactoryProviderTest, 'setup' | 'mockNative'>)
 {
     const config = {
--- projects/bespunky/angular-google-maps/overlays/testing/public-api.ts
+++ projects/bespunky/angular-google-maps/overlays/testing/public-api.ts
@@ -1,6 +1,6 @@
-export * from './helpers/overlay-wrapper-factory-provider-test-setup';
-export * from './helpers/overlay-native-factory-provider-test-production';
-export * from './helpers/overlay-wrapper-factory-provider-test-production';
+export * from './helpers/factories/setup/overlay-wrapper-factory-provider-test-setup';
+export * from './helpers/factories/overlay-native-factory-provider-test-production';
+export * from './helpers/factories/overlay-wrapper-factory-provider-test-production';
 
 export * from './mocks/mock-native-drawable-overlay';
 export * from './mocks/mock-drawable-overlay';
